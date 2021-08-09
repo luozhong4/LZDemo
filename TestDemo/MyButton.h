524d2bb8d6ba31e92e7df1ec41a881e37979f72d
luozhong <929269921@qq.com>
2021-08-09 17:59:30 +0800

    init project

---
+++ TestDemo/MyButton.h
@@ -0,0 +1,16 @@
+//
+//  MyButton.h
+//  MyButton
+//
+//  Created by 罗众 on 2021/8/9.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyButton : UIButton
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestDemo/MyButton.m
@@ -0,0 +1,20 @@
+//
+//  MyButton.m
+//  MyButton
+//
+//  Created by 罗众 on 2021/8/9.
+//
+
+#import "MyButton.h"
+
+@implementation MyButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
